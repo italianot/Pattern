b2740e2f9eb057a7e0cb2bdcd38991facd74629f
italianot <radioctiveboi@mail.ru>
2021-03-22 18:55:17 +0800

    acclivitas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,51 +12,50 @@ public class Main {
         int a;
         int b;
         int memory = 0;
-        int file = 0;
-        int folder = 0;
+        int fileCount = 0;
+        int folderCount = 0;
         System.out.println("Выберите тип создаваемого элемента: " +
                 "\n1. Текстовый файл " +
                 "\n2. Файл-изображение " +
                 "\n3. Папка " +
                 "\n4. Конец ");
 
-        while (in.hasNext()) {
+        Folder lastFolder = new Folder();
 
+        while (in.hasNext()) {
             a = in.nextInt();
-            int c = a;
             switch(a) {
                 case 1:
                     b = in.nextInt();
-                    Object File = new File();
-                    //Folder.addComponent(File); это работает, но нужно
-                    //чтобы файл добавлялся при создании пользователем папки
-                    //Как получить или запомнить что была создана папка?
-                        if (c==3){
-                            Folder.addComponent(File);
-                            System.out.println("В папке создан текстовый файл, размер = " + b);
-                        }else{
-                            System.out.println("Создан текстовый файл, размер = " + b);
-                        }
-                    file++;
+                    File File = new File();
+                    lastFolder.addComponent(File);
+                    System.out.println("Создан текстовый файл, размер = " + b);
+
+                    fileCount++;
                     memory = memory + b;
-                    //System.out.println("Создан текстовый файл, размер = " + b);
+
                     break;
                 case 2:
                     b = in.nextInt();
-                    Object Image = new Image();
-                    file++;
-                    memory = memory + b;
+                    Image Image = new Image();
+                    lastFolder.addComponent(Image);
                     System.out.println("Создан файл-изображение, размер = " + b);
+
+                    fileCount++;
+                    memory = memory + b;
                     break;
                 case 3:
-                    Object Folder = new Folder();
-                    folder++;
+                    Folder Folder = new Folder();
+                    lastFolder.addComponent(Folder);
+                    lastFolder = Folder;
+
+                    folderCount++;
                     System.out.println("Создана папка");
                     break;
                 case 4:
                     System.out.println("Объем памяти, занимаемый файловым " +
                             "хранилищем составил " + memory + " мегабайт. " +
-                            "Хранится: " + file + " файла и " + folder + " папки. ");
+                            "Хранится: " + fileCount + " файла и " + folderCount + " папки. ");
                     System.exit(0);
                     break;
                 default:
